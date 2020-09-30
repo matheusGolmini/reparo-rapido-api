999f747ce01dc11d7337e041f2d23236ce4cfec1
Matheus Fernando <matheus.golmini@wisereducacao.com>
2020-09-29 22:34:44 -0300

    typorm config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,25 +1,25 @@
-// import configEnv from './src/config'
+import configEnv from './src/config'
 
-// const configDB = configEnv().db_postgres
+const configDB = configEnv().db_postgres
 
-// module.exports = {
-//   type: 'postgres',
-//   host: configDB.host,
-//   port: Number(configDB.port),
-//   username: configDB.username,
-//   password: configDB.password,
-//   database: configDB.database,
-//   ssl: {
-//     rejectUnauthorized: false
-//   },
-//   entities: [
-//     configDB.entities
-//   ],
-//   migrations: [
-//     configDB.migrations
-//   ],
-//   cli: {
-//     migrationsDir: configDB.migrationsDir,
-//     entitiesDir: configDB.entitiesDir
-//   }
-// }
+module.exports = {
+  type: 'postgres',
+  host: configDB.host,
+  port: Number(configDB.port),
+  username: configDB.username,
+  password: configDB.password,
+  database: configDB.database,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  entities: [
+    configDB.entities
+  ],
+  migrations: [
+    configDB.migrations
+  ],
+  cli: {
+    migrationsDir: configDB.migrationsDir,
+    entitiesDir: configDB.entitiesDir
+  }
+}
