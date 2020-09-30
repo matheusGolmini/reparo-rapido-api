406d6a25bc0a50f4cb0c3fdb373204cfdb80653f
Matheus Fernando <matheus.golmini@wisereducacao.com>
2020-09-29 21:32:21 -0300

    remove typorm

--- src/db/config/index.ts
+++ src/db/config/index.ts
@@ -1,3 +1,26 @@
 import { createConnection } from 'typeorm'
+import config from '../../config/index'
 
-createConnection()
+const configDB = config().db_postgres
+
+createConnection({
+    type: 'postgres',
+    host: configDB.host,
+    port: Number(configDB.port),
+    username: configDB.username,
+    password: configDB.password,
+    database: configDB.database,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    entities: [
+        configDB.entities
+    ],
+    migrations: [
+        configDB.migrations
+    ],
+    cli: {
+        migrationsDir: configDB.migrationsDir,
+        entitiesDir: configDB.entitiesDir
+    }
+})
